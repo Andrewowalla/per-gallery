d65a8859adc69f53e87840bff345f73bdece4f22
Andrewowalla <andrewowalla@gmail.com>
2022-02-28 11:59:51 +0300

    Add a Model class Location

--- phoros/models.py
+++ phoros/models.py
@@ -3,9 +3,12 @@ from django.db import models
 from cloudinary.models import CloudinaryField
 
 # Create your models here.
+class Location(models.Model):
+    name = models.CharField(max_length=100)
+
 class Image(models.Model):
     image = CloudinaryField('image')
-    image_name = models.CharField(max_length= 30)
+    image_name = models.CharField(max_length= 100)
     image_description = models.TextField()
-    image_location = models.ForeignKey( on_delete=models.CASCADE)
+    image_location = models.ForeignKey(Location, on_delete=models.CASCADE)
     image_category = models.ForeignKey( on_delete=models.CASCADE)
