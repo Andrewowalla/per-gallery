ae5497cde17244efa4cb77b70b2f1b0a799013c6
Andrewowalla <andrewowalla@gmail.com>
2022-02-28 12:01:15 +0300

    Add a Model class Category

--- phoros/models.py
+++ phoros/models.py
@@ -6,9 +6,12 @@ from cloudinary.models import CloudinaryField
 class Location(models.Model):
     name = models.CharField(max_length=100)
 
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
 class Image(models.Model):
     image = CloudinaryField('image')
     image_name = models.CharField(max_length= 100)
     image_description = models.TextField()
     image_location = models.ForeignKey(Location, on_delete=models.CASCADE)
-    image_category = models.ForeignKey( on_delete=models.CASCADE)
+    image_category = models.ForeignKey(Category, on_delete=models.CASCADE)
