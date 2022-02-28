5023536ac9087b9c5b674dc7032528211570460d
Andrewowalla <andrewowalla@gmail.com>
2022-02-28 22:45:28 +0300

    Create a search_results view function

--- phoros/views.py
+++ phoros/views.py
@@ -13,3 +13,15 @@ def index(request):
     image  = Image.objects.all()
     
     return render(request, 'index.html', {"loaction":location, "category":category, "image":image})
+
+def search_results(request):
+  
+  if 'image' in request.GET and request.GET["image"]:
+    search_term = request.GET.get("image")
+    searched_images = Image.search_by_category(search_term)
+    message = f"{search_term}"
+    
+    return render(request,"search.html", {'message':message,'images':searched_images})
+  else:
+    message = "Please input a valid term"
+    return render(request,'search.html', {'message':message})
