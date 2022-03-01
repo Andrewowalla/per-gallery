8b0c7aa22be7c9d99b40c7533fddf95cae8f2150
Andrewowalla <andrewowalla@gmail.com>
2022-03-01 14:22:04 +0300

    Create Testcase for Location

--- phoros/tests.py
+++ phoros/tests.py
@@ -2,3 +2,15 @@ from django.test import TestCase
 from .models import Location, Category,tags
 
 # Create your tests here.
+class LocationTestClass(TestCase):
+    def setUp(self):
+        self.andrew = Location(name='kenya')
+        
+    def test_instance(self):
+        self.assertTrue(isinstance(self.andrew,Location))
+ 
+    def test_save_method(self):
+        self.andrew.save_location()
+        name = Location.objects.all()
+        self.assertTrue(len(name)>0)   
+
